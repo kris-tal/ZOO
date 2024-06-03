508a7975ba7c8ce53d591fd3ffd476087a65b32e
kris-tal <mdmgiglok@gmail.com>
2024-06-03 21:57:14 +0200

    opgat trengat

---
+++ data_generators/opiekunowie-gatunki.py
@@ -0,0 +1,24 @@
+import random
+import re
+
+def generuj_opiekunowie_gatunki(plik_pracownik_stanowisko, liczba_gatunkow):
+    opiekunowie = []
+
+    with open(plik_pracownik_stanowisko, 'r') as f:
+        for line in f:
+            match = re.search(r'\((\d+), (\d+)\)', line)
+            if match:
+                id_pracownika, id_stanowiska = match.groups()
+                if id_stanowiska == '4':
+                    opiekunowie.append(id_pracownika)
+
+    print("INSERT INTO opiekunowie_gatunki (id_pracownika, id_gatunku) VALUES")
+    
+    for i, id_pracownika in enumerate(opiekunowie):
+        print(f"({id_pracownika}, {i+1}),")
+
+    for id_gatunku in range(len(opiekunowie) + 1, liczba_gatunkow + 1):
+        id_pracownika = random.choice(opiekunowie)
+        print(f"({id_pracownika}, {id_gatunku}),")
+
+generuj_opiekunowie_gatunki('pracownik-stanowisko.sql', 100)
