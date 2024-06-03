1e1b1fdf57d0abe78c4a14cbb56e0bbe19d1e345
michal <michal.chlopicki@student.uj.edu.pl>
2024-06-03 19:38:11 +0200

    godziny_otwarcia insert

---
+++ data_generators/godziny_otwarcia.sql
@@ -0,0 +1,8 @@
+INSERT INTO godziny_otwarcia (dzien_tygodnia, otwarcie, zamkniecie) VALUES
+(1, '10:00'::time, '19:00'::time),
+(2, '10:00'::time, '19:00'::time),
+(3, '10:00'::time, '19:00'::time),
+(4, '10:00'::time, '19:00'::time),
+(5, '10:00'::time, '21:00'::time),
+(6, '8:00'::time, '21:00'::time),
+(7, '8:00'::time, '21:00'::time);
