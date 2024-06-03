fa644acbc871ec51733452876d00c62d1bdde191
michal <michal.chlopicki@student.uj.edu.pl>
2024-06-03 23:44:11 +0200

    nieobecnosci i niedyspozycyjnosc

--- data_generators/merge_inserts.py
+++ data_generators/merge_inserts.py
@@ -1,13 +1,19 @@
 w = open('all_inserts.sql', 'w')
 
 data = [
+    open('gatunki.sql').read(),
     open('godziny_otwarcia.sql').read(),
+    open('nieobecnosci_pracownikow.sql').read(),
+    open('niedyspozycja_zwierzat.sql').read(),
+    open('opiekunowie-gatunki.sql').read(),
     open('pracownicy.sql').read(),
-    open('pracownicy_godziny_pracy.sql'),
+    open('pracownicy_godziny_pracy.sql').read(),
     open('pracownik-stanowisko.sql').read(),
     open('stanowiska.sql').read(),
     open('strefy.sql').read(),
-    open('wybiegi.sql').read()
+    open('trenerzy-gatunki.sql').read(),
+    open('wybiegi.sql').read(),
+    open('zwierzeta.sql').read()
 ]
 
 for insert in data:
---
+++ data_generators/niebecnosci_pracownikow.py
@@ -0,0 +1,21 @@
+import random
+from faker import Faker
+import datetime
+
+fake = Faker('pl_PL')
+f = open('pracownicy.sql').read().split('\n')
+ile = len(f) - 2
+
+
+w = open('nieobecnosci_pracownikow.sql', 'w')
+w.write('INSERT INTO nieobecnosci_pracownikow (id_pracownika, data_od, data_do, powod) VALUES\n')
+
+nieobecnosci_id = []
+for i in range(1001):
+    id = random.randint(1, ile - 1)
+    data_od = datetime.date.today() + datetime.timedelta(days=random.randint(3, 365))
+    data_do = data_od + datetime.timedelta(days = random.randint(0, 14))
+    powod = fake.text(12)
+    w.write(f'({id}, \'{data_od}\', \'{data_do}\', NULL)')
+    if i == 1000: w.write(";\n")
+    else: w.write(",\n")
