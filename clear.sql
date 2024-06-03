4b10779c4d7fb23908f40bd14f3f123ed956e8e0
michal <michal.chlopicki@student.uj.edu.pl>
2024-06-03 17:19:59 +0200

    drop sequence i triggery

--- clear.sql
+++ clear.sql
@@ -19,15 +19,36 @@ DROP VIEW IF EXISTS godz_otwarcia CASCADE;
 
 -- funkcje trigger
 
-DROP TRIGGER IF EXISTS pojedynczy_wiersz ON godz_otwarcia CASCADE;
-DROP TRIGGER IF EXISTS akt_godz_otwarcia ON godz_otwarcia CASCADE;
+DROP TRIGGER IF EXISTS pojedynczy_wiersz ON godziny_otwarcia CASCADE;
+DROP TRIGGER IF EXISTS aktualizacja_godziny_otwarcia ON godziny_otwarcia CASCADE;
 DROP TRIGGER IF EXISTS change_zwierzeta ON zwierzeta CASCADE;
+DROP TRIGGER IF EXISTS dobry_pesel ON pracownicy CASCADE;
+DROP TRIGGER IF EXISTS akt_popisow ON plan_dnia CASCADE;
+DROP TRIGGER IF EXISTS check_plan_dnia ON plan_dnia CASCADE;
+DROP TRIGGER IF EXISTS check_popisy ON plan_dnia CASCADE;
+
 
 DROP FUNCTION IF EXISTS jeden CASCADE;
 DROP FUNCTION IF EXISTS check_godz_popisow CASCADE;
 DROP FUNCTION IF EXISTS update_licznosc_gatunku CASCADE;
 DROP FUNCTION IF EXISTS check_godz_otwarcia CASCADE;
+DROP FUNCTION IF EXISTS check_plan_dnia CASCADE;
+DROP FUNCTION IF EXISTS check_popisy CASCADE;
 
 -- zwykle funkcje
 
 DROP FUNCTION IF EXISTS dobry_pesel CASCADE;
+
+
+-- sekwencje
+
+DROP SEQUENCE IF EXISTS zwierzeta_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS wybiegi_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS strefy_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS stanowiska_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS pracownicy_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS popisy_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS plan_dnia_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS niedyspozycja_zwierzat_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS nieobecnosci_pracownikow_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS gatunki_id_seq CASCADE;
