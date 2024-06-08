10eaf1dd3c0bf93d46a15da7cacbb009b52a4365
michal <michal.chlopicki@student.uj.edu.pl>
2024-06-08 23:09:18 +0200

    dziala inserty

--- datagen/popisy.py
+++ datagen/popisy.py
@@ -12,12 +12,13 @@ with open('pracownicy_stanowiska.sql', 'r') as f:
 
 f = open('gatunki.sql').read().split('\n')
 ile_zwierzat = len(f) - 2
+nazwy = open('popisy.txt').read().split('\n')
 
 w = open('popisy.sql', 'w')
-w.write('INSERT INTO popisy (trener, gatunek, min_ilosc, min_poziom_umiejetnosci) VALUES\n')
+w.write('INSERT INTO popisy (trener, gatunek, min_ilosc, min_poziom_umiejetnosci, nazwa) VALUES\n')
 
 for i in range(401):
     w.write(f'({random.choice(trenerzy)}, {random.randint(1, ile_zwierzat)}, {random.randint(1, 7)}, '
-            f'{random.randint(0, 10)})')
+            f'{random.randint(0, 10)}, \'{random.choice(nazwy)}\')')
     if i < 400: w.write(',\n')
     else: w.write(';\n')
